e19682fe9a27ef93942e7392ed91394b4b980f12
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-09-07 16:11:03 +0200

    bring djvu metadata reader up to date

---
+++ TODO.md
@@ -0,0 +1,8 @@
+TODO
+====
+
+* do covers as png and 1024 px, instead of rely in the jpg covers provided by
+  the upstream plugin
+
+* Test djvu and pdf plugins
+
--- djvu-metadata/src/djvu.py
+++ djvu-metadata/src/djvu.py
@@ -17,37 +17,117 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import (unicode_literals, division)
-
-from .docmeta import DjvuMeta
 import os
+import shutil
+import subprocess
 
+from calibre import prints
+from calibre.ptempfile import TemporaryDirectory
 from calibre.customize import MetadataReaderPlugin
 from calibre.customize import MetadataWriterPlugin
 from calibre.ebooks.metadata.book.base import Metadata
+from calibre.utils.ipc.simple_worker import fork_job, WorkerError
+
+
+def get_djvu_metadata_worker(outputdir, get_cover):
+    ''' Read info dict and cover from a djvu file named src.djvu in outputdir.
+    Note that this function changes the cwd to outputdir and is therefore not
+    thread safe. Run it using fork_job. This is necessary as there is no safe
+    way to pass unicode paths via command line arguments.'''
+    os.chdir(outputdir)
+
+    try:
+        raw = subprocess.check_output(['djvused', '-e', 'print-meta', 'src.djvu'])
+
+    except: subprocess.CalledProcessError as e:
+        prints('djvused errored out with return code %d' % e.returncode)
+        return None
+
+    try:
+        raw = raw.decode('utf-8')
+    except UnicodeDecodeError:
+        prints('djvused returned no UTF-8 data')
+        return None
+
+    ret = {}
+
+    ma = re.search('Author\s+"(.*?)"', raw)
+    if ma: ret['author'] = ma.group(1)
+
+    mt = re.search('Title\s+"(.*?)"', raw)
+    if mt: ret['title'] = mt.group(1)
+
+    if get_cover:
+        size = 1024
+        try:
+            subprocess.check_call(['ddjvu', '-page=1', '-format=tiff', 'size=%dx%d' % (size, size), 'src.pdf', 'cover.pnm'])
+            subprocess.check_call(['pnmtopng', 'cover.pnm', 'cover.png'])
+        except subprocess.CalledProcessError as e:
+            prints('ddjvu errored out with return code: %d'%e.returncode)
+
+    return ret
+
+
+def set_djvu_metadata_worker(self, outputdir, title, author):
+    os.chdir(outputdir)
+    metadata = []
+    metadata.append('Title "%s"' % title)
+    metadata.append('Author "%s"' % author)
+    subprocess.check_output(['djvused', '-s', '-e', 'set-meta; %s' % '; '.join(metadata), 'src.djvu'])
+
+
+def get_djvu_metadata(outputdir, cover=True):
+    with TemporaryDirectory('_djvu_metadata_read') as djvupath:
+        stream.seek(0)
+        with open(os.path.join(djvupath, 'src.djvu'), 'wb') as f:
+            shutil.copyfileobj(stream, f)
+        try:
+            res = fork_job('calibre_plugins.djvu_metadata.djvu', 'get_djvu_metadata_worker', (djvupath, bool(cover)))
+        except WorkerError as e:
+            prints(e.orig_tb)
+            raise RuntimeError('Failed to run djvused')
+        info = res['result']
+        with open(res['stdout_stderr'], 'rb') as f:
+            raw = f.read().strip()
+            if raw:
+                prints(raw)
+        if not info:
+            raise ValueError('Could not read metadata from djvu')
+        covpath = os.path.join(pdfpath, 'cover.png')
+        cdata = None
+        if cover and os.path.exists(covpath):
+            with open(covpath, 'rb') as f:
+                cdata = f.read()
+
+    title = info.get('Title', None)
+    au = info.get('Author', None)
+    if au is None:
+        au = [_('Unknown')]
+    else:
+        au = string_to_authors(au)
+    mi = MetaInformation(title, au)
+
+    if cdata:
+        mi.cover_data = ('png', cdata)
+
+    return mi
 
 
 class DjvuMetadataReader(MetadataReaderPlugin):
     name                    = 'Get DJVU Metadata'
     description             = _('Reads djvu metadata, including cover')
     author                  = 'Abdó Roig-Maranges'
-    version                 = (1,2,0)
-    minimum_calibre_version = (0,8,0)
+    version                 = (1,3,0)
+    minimum_calibre_version = (2,66,0)
     file_types = ['djvu']
 
 
-    def get_metadata(self, stream, type):
-        mi = Metadata(_('Unknown'))
+    def get_metadata(self, stream, ftype):
+        if self.quick:
+            mi = get_djvu_metadata(stream)
+        else:
+            mi = get_djvu_metadata(stream, cover=True)
 
-        if hasattr(stream, 'name'): fname = os.path.abspath(stream.name)
-        else:                       return mi
-
-        djvum = DjvuMeta(fname)
-        meta = djvum.get_metadata()
-        if 'title' in meta.keys(): mi.title = meta['title']
-        if 'author' in meta.keys(): mi.authors = [au.strip() for au in meta['author'].split(',')]
-
-        if not self.quick:
-            mi.cover_data = ('png', djvum.get_cover())
-
+        mi.tags = []
         return mi
+
